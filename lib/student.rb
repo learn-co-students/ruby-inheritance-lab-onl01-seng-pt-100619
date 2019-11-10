023e1155bfb72b762b06c3d8dbd892b2007b72e3
Christina Gonzalez <cmgamway@gmail.com>
2019-11-10 20:53:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,3 @@
+require_relative "./user.rb"
 class Student < User
-
-  attr_accessor :first_name, :last_name
-
-  def initialize(:first_name, :last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
 end
