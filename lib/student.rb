83fef94354894cb2df1322601dc03a4db412aaf3
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-21 18:17:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require "pry"
+require_relative "./teacher.rb"
 
+class Student < User 
+
+  def initialize 
+    @knowledge = []
+  end 
+
+  def learn(str) 
+    str = "Ruby framework Rails gem bundle update"
+    @knowledge << str
+  end 
+  
+  def knowledge(strg="Javascript Ember Elixir knowledge")
+    
+    @knowledge << strg
+  end 
 end
